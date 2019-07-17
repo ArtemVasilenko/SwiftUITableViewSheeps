774d4af9c563ff0201c6dd276bd0c9e9d391bc07
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-07-17 20:19:03 +0300

    +

--- SwiftUITableViewSheeps/ContentView.swift
+++ SwiftUITableViewSheeps/ContentView.swift
@@ -1,11 +1,3 @@
-//
-//  ContentView.swift
-//  SwiftUITableViewSheeps
-//
-//  Created by Артем on 7/17/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView : View {
