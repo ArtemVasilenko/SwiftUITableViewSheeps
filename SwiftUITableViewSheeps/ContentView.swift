d6d171d3089bf96675d6c51af8efb1a9a32c2950
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-07-17 20:12:49 +0300

    Initial Commit

---
+++ SwiftUITableViewSheeps/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUITableViewSheeps
+//
+//  Created by Артем on 7/17/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
